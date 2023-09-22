679d35b39e2bee95e4bf04cf99d960620f297c52
Nim <104064871+Nim111111@users.noreply.github.com>
2023-09-22 15:12:09 +0530

    Add files via upload

---
+++ Day 8/src/home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './header';
+import './home.css';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import AddDonor from './components/AddDonor';
+import DonorList from './components/DonorList';
+
+function Home(){
+
+    return (
+        <div>
+            <Header />
+            <h1 id='t2'>Organ Donation Dashboard</h1>
+            <div className='dashboard'>
+                <Provider store={store}>
+                    <div>
+                        <AddDonor />
+                        <DonorList />
+                    </div>
+                </Provider>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
