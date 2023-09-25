490d27896ae7782d0a7cc4cf894b124c48b05807
Nim <104064871+Nim111111@users.noreply.github.com>
2023-09-25 10:17:22 +0530

    Delete Day 9 directory

--- Day 9/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import donorsReducer from '../features/donorsSlice';
-export const store = configureStore({
-  reducer: {
-    donors: donorsReducer,
-  },
-});
-
-export default store;
