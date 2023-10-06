bb3cf8835d6cf0d607ef644c8f8c2e2f48b36afc
Nim <104064871+Nim111111@users.noreply.github.com>
2023-10-06 12:06:49 +0530

    Add files via upload

---
+++ Day 11/src/main/java/com/example/organDonationBackend/OrganDonationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.organDonationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganDonationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganDonationBackendApplication.class, args);
+	}
+
+}
